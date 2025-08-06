d5ec4aa75c317668dc39c5fb0be820cad69e1089
Firepdx <sajjadcas32@gmail.com>
2025-08-06 15:35:53 +0500

    new client

---
+++ src/client/java/net/titanium/client/titanium_client/client/Titanium_clientClient.java
@@ -0,0 +1,10 @@
+package net.titanium.client.titanium_client.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Titanium_clientClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/titanium/client/titanium_client/Titanium_client.java
@@ -0,0 +1,10 @@
+package net.titanium.client.titanium_client;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Titanium_client implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
